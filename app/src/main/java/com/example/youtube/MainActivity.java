5f097b0cb9f7efcf378e75a1e36a31879ee1fba5
Dharmendeer <saferzone56@gmail.com>
2024-06-09 01:22:08 +0530

    THis is clne project

---
+++ app/src/main/java/com/example/youtube/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.youtube;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript for YouTube
+        webView.loadUrl("https://www.youtube.com/");
+    }
+}
